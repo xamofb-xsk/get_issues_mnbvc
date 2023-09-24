a3d8a71a361fbc644cb9d8726d50123645fb505f
vmiku99 <miku>
2023-09-24 18:08:01 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # get_issues_mnbvc
 `用于获取Issue`  
 `！！！需要填写 github_token 否则会限制请求次数`
+
+usage: python get_issue [-h] -p PATH -t TOKEN
--- get_issue.py
+++ get_issue.py
@@ -2,107 +2,128 @@ import json
 import os
 import random
 import time
-from concurrent.futures import ThreadPoolExecutor
-
 import requests
 from tqdm import tqdm
-
+import argparse
 SOURCE_PATH = './20230301/github.20230301.1.代码元数据/'  # 源路径
 TARGET_PATH = './output'  # 目的路径
-THREADS = 4  # 线程池大小
+TMP_PATH = './tmp' # 存放爬取记录
 
-github_tokens = [
-    ""]
-headers = [{"Authorization": f"token {github_token}"}
-           for github_token in github_tokens]
-
+github_token = ""
+header = {"Authorization": f"token {github_token}"}
 
 def get_jsonl_filenames(directory):
-    # 获取目录下以jsonl结尾的文件，返回绝对路径列表
+    # 获取目录下以jsonl结尾的文件，返回相对路径列表
     jsonl_filenames = []
     for filename in os.listdir(directory):
         if filename.endswith('.jsonl'):
-            jsonl_filenames.append(os.path.join(directory, filename))
+            jsonl_filenames.append(filename)
     return jsonl_filenames
 
 
 def get_data(filename):
     # 解析jsonl
-    data = []
     with open(filename, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        for line in tqdm(lines, desc="Processing"):  # 使用 tqdm 来添加进度条
-            json_obj = json.loads(line)
-            data.append(json_obj)
-        return data
+        for line in f:
+            try:
+                yield json.loads(line)
+            except Exception as e:
+                print(e)
 
-
-def get_issues(metadata):
-    # 根据元信息获取issues
-    if os.path.exists(os.path.join(TARGET_PATH, f'{metadata["id"]}.json')):
-        return
-    total_issues = []
-    page = 1
-    while True:
+def get(url, params=None):
+    # requests.get， 并防止token超限
+    print(f'get {url}')
+    for i in range(3):
         try:
-            response = requests.get(metadata['url'] + '/issues', headers=random.choice(
-                headers), params={"page": page, "state": "all"})
+            response = requests.get(url, headers=header, params=params)
             limit = response.headers.get("X-RateLimit-Limit")
             remaining = response.headers.get("X-RateLimit-Remaining")
             reset = response.headers.get("X-RateLimit-Reset")
             current_timestamp = int(time.time())
             time_diff = int(reset) - current_timestamp
-            print(
-                f"ID: {metadata['id']} PAGE: {page} TOKEN REMAINING {remaining} / {limit} ISSUES: {metadata['url']}")
-            if int(remaining) > 20:
-                if response.status_code == 200:
-                    issues = response.json()
-                    if not issues:
-                        break  # 没有更多数据了，退出循环
-                    total_issues.extend(issues)
-                else:
-                    print(
-                        f"Failed to fetch issues. Status code: {response.status_code}")
+            if response.status_code == 200:
+                data = response.json()
             else:
-                time.sleep(time_diff)
-            page += 1
+                data = []
+            if int(remaining) <= 20:
+                time.sleep(time_diff)            
+            return data
+        except Exception as e:
+            print(e)
+            continue
+
+def get_issues(metadata, record_file):
+    record = {}
+    if os.path.exists(record_file):
+        with open(record_file, 'r') as f:
+            try:
+                record = json.load(f)
+            except Exception as e:
+                print(e)
+        
+    if metadata['id'] in record:
+        return
+
+    with open(os.path.join(TARGET_PATH, f'{metadata["id"]}.jsonl'), 'w', encoding='utf-8') as f:
+        pass
+
+    page = 1
+    while True:
+        try:
+            issues = get(metadata['url'] + '/issues', params={"page": page, "state": "all"})
+            add_comments(metadata, issues)
+            if issues:
+                page += 1
+            else:
+                break
         except Exception as e:
             print(e)
             break
-    add_comments(total_issues)
-    write_to_file({
-        'id': metadata['id'],
-        'issues': total_issues
-    })
-    return total_issues
+
+    record[metadata['id']] = {'get_time': time.time()}
+    with open(record_file, 'w') as f:
+        json.dump(record, f)
 
 
-def add_comments(issues):
+
+def add_comments(metadata, issues):
     # 增加评论
     for issue in issues:
         issue['comments_data'] = []
         if issue['comments']:
             comments_url = issue['comments_url']
-            comments_response = requests.get(
-                comments_url, headers=random.choice(headers))
-            if comments_response.status_code == 200:
-                issue['comments_data'].extend(comments_response.json())
-            else:
-                print(
-                    f"Failed to fetch comments. Status code: {comments_response.status_code}")
-
-
-def write_to_file(data):
-    if not os.path.exists(TARGET_PATH):
-        os.makedirs(TARGET_PATH)
-    with open(os.path.join(TARGET_PATH, f'{data["id"]}.json'), 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False)
+            comments_data = get(comments_url)
+            if comments_data:
+                issue['comments_data'].extend(comments_data)
+        write_to_file(metadata, issue)
+        
+def write_to_file(metadata, issue):
+    with open(os.path.join(TARGET_PATH, f'{metadata["id"]}.jsonl'), 'a', encoding='utf-8') as f:
+        f.write(json.dumps(issue, ensure_ascii=False) + '\n')
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            prog='python get_issue.py',
+            description='GET MNBVC ISSUES',
+            epilog='Text at the bottom of help')
+    parser.add_argument('-p', '--path', help='meta jsonl path', required=True)
+    parser.add_argument('-t', '--token', help='github token like github_pat_*', required=True)
+    
+
+    args = parser.parse_args()
+    SOURCE_PATH = args.path
+    headers = {"Authorization": f"token {args.token}"}
+
+    if not os.path.exists(TARGET_PATH):
+        os.makedirs(TARGET_PATH)
+
+    if not os.path.exists(TMP_PATH):
+        os.makedirs(TMP_PATH)
+
     jsonl_filenames = get_jsonl_filenames(SOURCE_PATH)  # 获取文件名
     for filename in jsonl_filenames:
-        metadatas = get_data(filename)
-        with ThreadPoolExecutor(THREADS) as pool:
-            for metadata in metadatas:
-                pool.submit(get_issues, metadata)
+        abs_filename = os.path.join(SOURCE_PATH, filename)
+        record_file = os.path.join(TMP_PATH, filename.replace('jsonl', 'json'))
+        for metadata in get_data(abs_filename):
+            get_issues(metadata, record_file)
